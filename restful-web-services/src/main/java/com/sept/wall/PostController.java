417e1dc8a6636ecadc73418638d864d8516b6241
s3664099 <s3664099@student.rmit.edu.au>
2019-08-24 19:18:17 +1000

    completed controller class for this sprint

--- restful-web-services/src/main/java/com/sept/wall/PostController.java
+++ restful-web-services/src/main/java/com/sept/wall/PostController.java
@@ -4,47 +4,73 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-//Controller
+//sets up the middleware section of the system
 @RestController
 @CrossOrigin(origins="http://localhost:4200")
 
 public class PostController {
 	
-	PostHardCode code = new PostHardCode();
-	
-	@GetMapping(path = "users/Post")
-	public Post SendPost() {
-		
-		return new Post(1,"This is a post", LocalDate.now());
-		
-	}
-	
-	@GetMapping(path = "users/{username}/Post/id")
-	public ResponseEntity<Void> doPost() {
-		
-		return ResponseEntity.notFound().build();
-	}
-	
-	@GetMapping(path = "users/{username}/Post")
-	public List<Post> SendPostList(@PathVariable String username) {
-		
-		List<Post> listToSend = new ArrayList<Post>();
-		List<Post> listToSearch = code.getAll();
-		
-		for (Post post:listToSearch) {
-			if (post.getDisplay())
-				listToSend.add(post);
-		}
+	//creates a constructor to be used for the process
+	@Autowired
+
+	//At this stage this is hardcoded
+	//However, it will be the class which ultimately sends the SQL through
+	//to the database
+	PostHardCode code;
+			
+	//method to get a list of all visible posts
+	@GetMapping(path = "users/{studentid}/Post")
+	public List<Post> sendVisiblePostList(@PathVariable int studentid) {
+
+		List<Post> listToSend = code.getAll(studentid);
 		
 		return listToSend;
 		
 	}
+	
+	//method to get a list of all posts
+	@GetMapping(path = "users/{studentid}/Post")
+	public List<Post> sendPostList(@PathVariable int studentid) {
+
+		List<Post> listToSend = code.getAllVisible(studentid);
+		
+		return listToSend;
+		
+	}
+	
+	
+	//delete post by id
+	@GetMapping(path = "users/{studentid}/Post/{id}")
+	public void DeletePost(@PathVariable int studentid, int id) {
+		
+		code.deletePost(studentid, id);
+		
+	}
+	
+	//get post by id
+	@GetMapping(path = "users/{studentid}/Post/{id}")
+	public Post getPost(@PathVariable int studentid, int id) {
+		
+		return code.findPostbyId(studentid, id);
+		
+	}
+	
+	//add a new post to the wall
+	@GetMapping(path = "users/{studentid}/Post/{post}")
+	public void newPost(@PathVariable int studentid, String post) {
+		
+		code.add(studentid, post);
+		
+	}
+	
+	
 	
 	
 
--- restful-web-services/src/main/java/com/sept/wall/PostHardCode.java
+++ restful-web-services/src/main/java/com/sept/wall/PostHardCode.java
@@ -4,27 +4,52 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+//This needs to be added to allow the autowired to function
+@Service
 public class PostHardCode {
 	
+	//creates a list of posts and sets the last value
 	private static List<Post> postlist = new ArrayList<Post>();
 	private int postValue = 39;
 	
-	public PostHardCode() {
+	//creates and adds some posts to the list
+	static {
 		postlist.add(new Post (34,"This is cool",LocalDate.now()));
 		postlist.add(new Post (38,"Multiple Choice Sucks",LocalDate.now()));
 		postlist.add(new Post (21,"Not cool dude, not cool",LocalDate.now()));
 		postlist.add(new Post (30,"Yeah, just that",LocalDate.now()));
 	}
 	
-	public List getAll() {
+	//returns a list of all posts
+	public List getAll(int studentId) {
 		return postlist;
 	}
 	
-	public void add(String post) {
-		postlist.add(new Post(postValue, post, LocalDate.now()));
+	//returns a list of all visible posts
+	public List getAllVisible(int studentId) {
+		
+		List<Post> listToSend = new ArrayList<Post>();
+		
+		//iterates through list of posts and removes the ones 
+		//that aren't visible
+		for (Post post:postlist) {
+			if (post.getDisplay())
+				listToSend.add(post);
+		}
+		
+		return listToSend;
 	}
 	
-	public Post findPostbyId(int id) {
+	//adds a post to the list
+	public void add(int studentId, String post) {
+		postlist.add(new Post(postValue, post, LocalDate.now()));
+		postValue++;
+	}
+	
+	//returns a post based on the id passed through to it
+	public Post findPostbyId(int studentId, int id) {
 		Post foundpost = null;
 		
 		for (Post post: postlist) {
@@ -35,12 +60,27 @@ public class PostHardCode {
 		return foundpost;
 	}
 	
-	public void deletePost(int id) {
+	//deletes a post based on the id of the post
+	public void deletePost(int studentId, int id) {
 		
 		for (Post post: postlist) {
 			if (id == post.getId())
 				post.setDisplay();
 		}
+		
+	}
+	
+	//get a post based on the id of the post
+	public Post getPostById(int studentId, int id) {
+		
+		Post postToReturn = null;
+		
+		for (Post post: postlist) {
+			if (id == post.getId())
+				postToReturn = post;
+		}
+		
+		return postToReturn;
 		
 	}
 
