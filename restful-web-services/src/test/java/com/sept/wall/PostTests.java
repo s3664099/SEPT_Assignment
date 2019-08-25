00c5cbae30ddf756915ee9d04ef59ab6f5fa79e9
s3664099 <s3664099@student.rmit.edu.au>
2019-08-25 13:10:57 +1000

    Test Cases updates and spirateam implemented

--- restful-web-services/src/test/java/com/sept/wall/PostTests.java
+++ restful-web-services/src/test/java/com/sept/wall/PostTests.java
@@ -1,28 +1,42 @@
 package com.sept.wall;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
-import java.time.LocalDate;
+import java.util.Date;
 
 import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
+import com.example.demo.Post;
+import com.example.demo.User;
+import com.inflectra.spiratest.addons.junitextension.SpiraTestCase;
+import com.inflectra.spiratest.addons.junitextension.SpiraTestConfiguration;
 
-public class PostTests {
+@SpiraTestConfiguration(
+        //following are REQUIRED
+        url = "https://rmit-university.spiraservice.net",
+        login = "s3664099",
+        password = "",
+        projectId = 379
+)
+
+class PostTests {
 
 	static Post post;
 	static User user;
 	static User user1;
-	static LocalDate date;
+	static Date date;
 	
-	@BeforeClass
+	@BeforeAll	
 	public static void setUpBeforeClass() throws Exception {
+		date = new Date();
 		user = new User("Mr Bean",3999999,"Mr","Bean");
-		post = new Post(user, 001, "Hello, this is a post");
-		date = LocalDate.now();
-		
+		post = new Post(user.getStudentId(), 001, "Hello, this is a post", date);
 		user1 = new User("Mr Bean",001,"Mr","Bean");
 		post.likePost(user1);
 		user1 = new User("Mr Bean",002,"Mr","Bean");
@@ -37,16 +51,19 @@ public class PostTests {
 	}
 
 	@Test
+	@SpiraTestCase(testCaseId = 7565)
 	public void testDate() {
 		assertEquals(date,post.getDate());
 	}
 	
 	@Test
+	@SpiraTestCase(testCaseId = 8448)
 	public void testDisplayTrue() {
 		assertTrue(post.getDisplay());
 	}
 		
 	@Test
+	@SpiraTestCase(testCaseId = 8449)
 	public void testDisplayToggle() {
 		
 		post.setDisplay();
@@ -54,12 +71,14 @@ public class PostTests {
 	}
 	
 	@Test
+	@SpiraTestCase(testCaseId = 8450)
 	public void testMessage() {
 		
 		assertEquals("Hello, this is a post", post.getPost());
 	}
 	
 	@Test
+	@SpiraTestCase(testCaseId = 8451)
 	public void testEditPost() {
 
 		post.editPost("Truly, this is a post");
@@ -67,11 +86,13 @@ public class PostTests {
 	}
 	
 	@Test
+	@SpiraTestCase(testCaseId = 8452)
 	public void testNoLikes() {
 		assertEquals(5,post.getNoLikes());
 	}
 	
 	@Test
+	@SpiraTestCase(testCaseId = 8454)
 	public void testAddLike() {
 
 		user1 = new User("Mr Bean",010,"Mr","Bean");
@@ -80,13 +101,14 @@ public class PostTests {
 	}
 	
 	@Test
+	@SpiraTestCase(testCaseId = 8455)
 	public void testRejectLike() {
 		
 		user1 = new User("Mr Bean",005,"Mr","Bean");
 		assertTrue(post.likePost(user1));
 	}
 	
-	@After
+	@AfterEach
 	public void resetDisplay() {
 		
 		post.editPost("Hello, this is a post");
