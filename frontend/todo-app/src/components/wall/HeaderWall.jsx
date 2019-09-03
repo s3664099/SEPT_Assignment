744fe9199f10434571a7f181de0180f88fd64fc5
Jas <s3691487@student.rmit.edu.au>
2019-09-04 02:34:11 +1000

    rest request for user name

--- frontend/todo-app/src/components/wall/HeaderWall.jsx
+++ frontend/todo-app/src/components/wall/HeaderWall.jsx
@@ -2,11 +2,57 @@ import React, {Component} from 'react'
 import './HeaderWall.css'
 import Search from './Search.jsx'
 
+var myInit ={
+    method:'GET',
+    headers:{
+        'Content-Type':'application/json'
+    },
+    mode:'cors',
+    cache:'default'
+};
+let myRequest = new Request('http://localhost:4200/profile', myInit);
+
 class HeaderWall extends Component {
 
+    constructor(props){
+        super(props);
+        this.state ={
+            error:null,
+            isLoaded:false,
+            items:[]
+        };
+    }
+
+    componentDidMount(){
+        fetch(myRequest)
+        .then(res =>res.json())
+        .then(
+            (result) =>{
+                this.setState({
+                    isLoaded:true,
+                    items: result
+                });
+            },
+        
+        (error)=>{
+            this.setState({
+                isLoaded:true,
+                error
+            });
+        }
+        )
+    }
+
     render(){
-        const userName = "testUser";
-        return (
+        const {error, isLoaded, items}=this.state;
+        const userName = items.name;
+        if(error){
+            return <div>Error:{error.message}</div>
+        }else if(!isLoaded){
+            return <div>Loading...</div>
+        }else{
+             return (
+                 
             <header>
                 <nav className="wallnav">
                     <div className="wallLogo">
@@ -24,6 +70,8 @@ class HeaderWall extends Component {
                 </nav>
             </header>
         )
+        }
+       
     }
 }
 
