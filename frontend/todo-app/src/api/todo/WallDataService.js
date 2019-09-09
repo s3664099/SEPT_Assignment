131be71cdad67eb37acc5cb74e3f09cb5e60c09b
David Sarkies <41358081+s3664099@users.noreply.github.com>
2019-09-09 13:50:53 +1000

    Merged Wall Data Service

---
+++ frontend/todo-app/src/api/todo/WallDataService.js
@@ -0,0 +1,43 @@
+import axios from 'axios'
+import { API_URL, JPA_API_URL } from '../../Constants'
+
+class WallDataService {
+
+
+    retrieveAllVisiblePosts(name) {
+        //console.log('executed service')
+        return axios.get(`${JPA_API_URL}/users/${studentId}/Posts`);
+    }
+
+    //If the request is successful, a message advising the success is sent
+    //otherwise a bad requet is recieved and sent
+    deletePost(name, id) {
+
+    	//console.log('executed service')
+        return axios.get(`${JPA_API_URL}/users/${studentId}/Posts/${id}`, result);
+    }
+
+    retrievePost(name, id) {
+        //console.log('executed service')
+        return axios.get(`${JPA_API_URL}/users/${studentId}/Posts/${id}`);
+    }
+
+    //the new post is sent to the server. If the student Ids don't match
+    //a bad request is returned
+    createPost(name, post) {
+        //console.log('executed service')
+        return axios.post(`${JPA_API_URL}/users/${studentId}/Posts`, post);
+    }
+
+    createComment(name, post, parentPostId) {
+    //console.log('executed service')
+        return axios.post(`${JPA_API_URL}/users/${studentId}/Posts/${parentPostId}`, post);
+    }
+
+    editPost(name, post) {
+    	return axios.post(`${JPA_API_URL}/users/${studentId}/Posts`, post);
+    }
+
+
+
+}
