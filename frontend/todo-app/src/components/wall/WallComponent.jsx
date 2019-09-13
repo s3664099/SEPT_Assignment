c53a6476bb0ce2e653b4cf2a241bdf0070832c65
Jasmine <s3691487@student.rmit.edu.au>
2019-09-13 19:35:51 +1000

    new Wall component

---
+++ frontend/todo-app/src/components/wall/WallComponent.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+
+class wallComponent extends Component {
+    render() {
+      return (
+        <div className="Wall">
+        <p>test wall ...</p>
+        </div>
+      );
+    }
+  }
+export default wallComponent;
