6296a412cdbeadd022ca9842ae3fadac247ff234
s3689650 <s3689650@student.rmit.edu.au>
2019-09-13 20:40:26 +1000

    Fixed SSL Error

--- restful-web-servicesinitial/src/main/resources/data.sql
+++ restful-web-servicesinitial/src/main/resources/data.sql
@@ -1,8 +1,17 @@
-insert into todo(id, username,description,target_date,is_done)
-values(10001, 'sept', 'Learn JPA', sysdate(), false);
+DROP TABLE todo;
 
-insert into todo(id, username,description,target_date,is_done)
-values(10002, 'sept', 'Learn Data JPA', sysdate(), false);
+CREATE TABLE todo (
+	id				INT	UNSIGNED	NOT NULL,
+	username 	VARCHAR(30),
+	description	VARCHAR(1000),
+	target_date	TIMESTAMP,
+	is_done		BOOLEAN,
+	PRIMARY KEY (id)
+);
 
-insert into todo(id, username,description,target_date,is_done)
-values(10003, 'sept', 'Learn Microservices', sysdate(), false);
+INSERT INTO todo(id, username, description, target_date, is_done)
+VALUES(10001, 'sept', 'Learn JPA', sysdate(), false);
+INSERT INTO todo(id, username, description, target_date, is_done)
+VALUES(10002, 'sept', 'Learn Data JPA', sysdate(), false);
+INSERT INTO todo(id, username, description, target_date, is_done)
+VALUES(10003, 'sept', 'Learn Microservices', sysdate(), FALSE);
