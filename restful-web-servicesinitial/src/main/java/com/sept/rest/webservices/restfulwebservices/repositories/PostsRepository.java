44e016f24638ac466d393ddf55e047a4398e52eb
s3689650 <s3689650@student.rmit.edu.au>
2019-09-18 20:37:17 +1000

    Finished writing mappings for post manipulation

--- restful-web-servicesinitial/src/main/java/com/sept/rest/webservices/restfulwebservices/repositories/PostsRepository.java
+++ restful-web-servicesinitial/src/main/java/com/sept/rest/webservices/restfulwebservices/repositories/PostsRepository.java
@@ -9,5 +9,9 @@ import com.sept.rest.webservices.restfulwebservices.entities.Posts;
 
 @Repository
 public interface PostsRepository extends CrudRepository<Posts, Long> {
+
+	List<Posts> findByOwner_idAndDeletedFalse(int studentID);
+
+	Posts findByIdAndDeletedFalse(int id);
 	
 }
--- restful-web-servicesinitial/src/main/java/com/sept/rest/webservices/restfulwebservices/wall/PostsResource.java
+++ restful-web-servicesinitial/src/main/java/com/sept/rest/webservices/restfulwebservices/wall/PostsResource.java
@@ -11,91 +11,99 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.sept.rest.webservices.restfulwebservices.entities.Posts;
-import com.sept.rest.webservices.restfulwebservices.entities.Students;
 import com.sept.rest.webservices.restfulwebservices.repositories.PostsRepository;
 import com.sept.rest.webservices.restfulwebservices.repositories.StudentsRepository;
 
-//sets up the middleware section of the system
+// Class containing all mappings related to posts on a wall
 @RestController
 @CrossOrigin(origins="http://localhost:4200")
 public class PostsResource {
 	
-	//creates a constructor to be used for the process
-	
-
-	//At this stage this is hardcoded
-	//However, it will be the class which ultimately sends the SQL through
-	//to the database
-//	private PostIDHardCode backEndDataBase;
-	//DBLink code;
-
 	@Autowired
-	private PostsRepository postRepository;
-	//private StudentRepository studentRepository;
 	
-	//method to get a list of all visible posts
+	private PostsRepository postsRepository;
+	private StudentsRepository studentsRepository;
+	
+	// Mapping to get a list of all undeleted posts for a given wall
+	//		When specific id can be determined rework to fetch appropriate wall
 	@GetMapping(path = "/jpa/users/{username}/wall")
 	public List<Posts> sendVisiblePostList(@PathVariable String username) {
-		System.out.println("axios request recieved");
-		//int studentID = studentRepository.findByDisplay_name(username).getStudentID();
-		//return postRepository.findByStudentId(studentid);
-		//return postRepository.findByOwner_id(studentID);
-		return (List<Posts>) postRepository.findAll();
+		int studentId = studentsRepository.findByDisplay_name(username).getStudentID();
+		return postsRepository.findByOwner_idAndDeletedFalse(studentId);
 	}
 	
-//	//delete post by id
-//	@DeleteMapping(path = "users/{username}/Posts/{id}")
-//	public ResponseEntity<String> DeletePost(@PathVariable int studentid, @PathVariable int id) {
-//		
-//		//Attempts to delete the post
-//		//postRepository.deleteById(id);
-//		postRepository.deletePost(studentid, id);
-//
-//		//otherwise the successful request is returned
-//		return ResponseEntity.noContent().build();
-//		
-//	}
-//	
-//	//add a new post to the wall
-//	@PostMapping(path = "users/{studentid}/Posts")
-//	public List newPost(@PathVariable int studentid, @RequestBody PostID post) {
-//		
-//		Date date = new Date();
-//		
-//		post.setUser(studentid);
-//		post.setDate(date);
-//		
-//		backEndDataBase.addPostToWall(post);
-//
-//		/*
-//		PostID createdPost = postRepository.save(post);
-//
-//		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
-//				.path("/{id}").buildAndExpand(createdPost.getId()).toUri();
-//		*/
-//		
-//		return backEndDataBase.getAllVisible(studentid);		
-//		
-//	}
-//	
-//	//edit post on wall
-//	@GetMapping(path = "users/{studentid}/Posts/{id}")
-//	public List editPost(@PathVariable int studentId, @PathVariable int id, @RequestBody PostID temppost ) {
-//		
-//		PostID post = backEndDataBase.findPostbyId(studentId, id);
-//		
-//		post.editPost(temppost.getPost());
-//		post.setUser(studentId);
-//		
-//		//PostID postUpdated = postRepository.save(post);
-//		
-//		return backEndDataBase.getAllVisible(studentId);
-//	}
-//	
+	// Mapping to get a specific post if undeleted
+	@GetMapping(path = "/jpa/users/{username}/post/{postId}")
+	public Posts getPost(@PathVariable String username, @PathVariable int postId) {
+		return postsRepository.findByIdAndDeletedFalse(postId);
+	}
+	
+	// Mapping to add a new post to the wall
+	@PostMapping(path = "/jpa/users/{username}/wall/{wallId}")
+	public ResponseEntity<Void> newPost(@PathVariable String username, @PathVariable int wallId, @RequestBody Posts post) {
+		
+		// Ensure new post will be appended to users wall and not someone else's
+		post.setOwnerID(studentsRepository.findByDisplay_name(username).getStudentID());
+			
+		// Store new post in database
+		Posts newPost = postsRepository.save(post);
+			
+		// Append the new Posts id to the url path
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newPost.getPostID()).toUri();
+			
+		// Construct an entity to return
+		return ResponseEntity.created(uri).build();
+	}
+	
+	// Mapping to edit a post. Returns unedited post with unauthorized status if user is not posts owner
+	@PutMapping("/jpa/users/{username}/post/{postId}")
+	public ResponseEntity<Posts> updateTodo(
+			@PathVariable String username,
+			@PathVariable int postId, @RequestBody Posts post) {
+		
+		// Check that this is post by the user
+		if (studentsRepository.findByDisplay_name(username).getStudentID() == post.getOwnerID()) {
+
+			// Store edited post in database
+			Posts editedPost = postsRepository.save(post);
+			
+			// Construct an entity to return
+			return new ResponseEntity<Posts>(editedPost, HttpStatus.OK);
+		}
+		
+		// User not Authorized to edit post. Unchanged post returned with appropriate status
+		return new ResponseEntity<Posts>(post, HttpStatus.UNAUTHORIZED);
+	}
+	
+	// Mapping to remove a post from the wall
+	@DeleteMapping("/jpa/users/{username}/post/{postId}")
+	public ResponseEntity<Void> deletePost(
+			@PathVariable String username, @PathVariable int postId) {
+
+		// Retrieve post to be removed from the wall
+		Posts post = postsRepository.findByIdAndDeletedFalse(postId);
+		
+		// Check that user has permission to delete post
+		if (studentsRepository.findByDisplay_name(username).getStudentID() == post.getOwnerID()) {
+			
+			// Set post to not be displayed
+			post.setDeleted(true);
+			
+			// Update database
+			postsRepository.save(post);
+			
+			// Construct an entity to return
+			return ResponseEntity.noContent().build();
+		}
+		
+		// User not authorized to delete post. Respond with appropriate status
+		return new ResponseEntity<Void>(HttpStatus.UNAUTHORIZED);
+	}
 
 }
