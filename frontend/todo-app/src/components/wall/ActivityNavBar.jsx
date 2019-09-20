e2655da808dc1b3e6e452c5e0f9e786006a0a1de
Jasmine <s3691487@student.rmit.edu.au>
2019-09-20 19:33:12 +1000

    activitySideBar

---
+++ frontend/todo-app/src/components/wall/ActivityNavBar.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import './SideNavBar.css'
+import Popup from "reactjs-popup";
+
+class ActivityNavBar extends Component {
+    render() {
+        return (
+            <div className="activityNavBar">
+            <Popup trigger={<button className="newPostButton"> Create New Post </button>}
+      modal
+      closeOnDocumentClick>
+            <div>
+            <p>Create New Post</p>
+            <form action="/action_page.php">
+              <textarea name="message" rows="10" cols="100" placeholder="Write your status..."></textarea>
+              <br/>
+              <input type="submit"/>
+              </form>
+            </div>
+              </Popup>
+                <a href="#">nav2</a>
+                <a href="#">nav3</a>
+                <a href="#">nav4</a>
+            </div>
+        )
+    }
+}
+
+export default ActivityNavBar
--- frontend/todo-app/src/components/wall/HeaderWall.jsx
+++ frontend/todo-app/src/components/wall/HeaderWall.jsx
@@ -2,55 +2,15 @@ import React, {Component} from 'react'
 import './HeaderWall.css'
 import Search from './Search.jsx'
 
-var myInit ={
-    method:'GET',
-    headers:{
-        'Content-Type':'application/json'
-    },
-    mode:'cors',
-    cache:'default'
-};
-let myRequest = new Request('http://localhost:3000/profile', myInit);
 
 class HeaderWall extends Component {
 
     constructor(props){
         super(props);
-       /* this.state ={
-            error:null,
-            isLoaded:false,
-            items:[]
-        };*/
     }
-/*
-    componentDidMount(){
-        fetch(myRequest)
-        .then(res =>res.json())
-        .then(
-            (result) =>{
-                this.setState({
-                    isLoaded:true,
-                    items: result
-                });
-            },
-        
-        (error)=>{
-            this.setState({
-                isLoaded:true,
-                error
-            });
-        }
-        )
-    }*/
+
 
     render(){
-       // const {error, isLoaded, items}=this.state;
-       // const userName = items.name;
-       /* if(error){
-            return <div>Error:{error.message}</div>
-        }else if(!isLoaded){
-            return <div>Loading...</div>
-        }else{*/
              return (
                  
             <header>
@@ -73,7 +33,6 @@ class HeaderWall extends Component {
         }
        
     }
-//}
 
 
 
