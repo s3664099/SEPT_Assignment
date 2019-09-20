7789ecc34f04c8b675c2af072b8012ae6b099ea7
Raphael Wong <s3735236@student.rmit.edu.au>
2019-09-20 21:32:09 +1000

    add activity nav bar

--- frontend/todo-app/src/components/wall/WallComponent.jsx
+++ frontend/todo-app/src/components/wall/WallComponent.jsx
@@ -4,15 +4,17 @@ import HeaderWall from './HeaderWall';
 import AuthenticationService from '../todo/AuthenticationService'
 import SideNavBar from './SideNavBar';
 import PostContainer from './PostContainer';
+import ActivityNavBar from './ActivityNavBar'
 
 class wallComponent extends Component {
-    
+
     render() {
         const userName = AuthenticationService.getLoggedInUserName();
       return (
         <div className="Wall">
         <HeaderWall name = {userName}/>
         <SideNavBar />
+        <ActivityNavBar/>
         <PostContainer/>
         </div>
       );
