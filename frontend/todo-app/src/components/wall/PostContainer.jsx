e3dcec4563e30e41445852d98eee0382897eec21
Raphael Wong <s3735236@student.rmit.edu.au>
2019-09-30 22:25:23 +1000

    final css hopefully

--- frontend/todo-app/src/components/wall/PostContainer.jsx
+++ frontend/todo-app/src/components/wall/PostContainer.jsx
@@ -9,111 +9,112 @@ import moment from 'moment'
 
 
 class PostContainer extends Component {
-    
-    constructor(props){
-        console.log(" post constructor")
-        super(props)
-        this.state ={
-            posts:[]
-        }
-        this.refreshPosts=this.refreshPosts.bind(this)
-        this.addPostClicked = this.addPostClicked.bind(this)
-        this.editPostButton = this.editPostButton.bind(this)
-        this.deletePostButton = this.deletePostButton.bind(this)
+
+  constructor(props){
+    console.log(" post constructor")
+    super(props)
+    this.state ={
+      posts:[]
     }
-    
-    componentWillUnmount() {
-        console.log('componentWillUnmount on wall')
-    }
+    this.refreshPosts=this.refreshPosts.bind(this)
+    this.addPostClicked = this.addPostClicked.bind(this)
+    this.editPostButton = this.editPostButton.bind(this)
+    this.deletePostButton = this.deletePostButton.bind(this)
+  }
 
-    shouldComponentUpdate(nextProps, nextState) {
-        console.log('shouldComponentUpdate on wall')
-        console.log(nextProps)
-        console.log(nextState)
-        return true
-    }
+  componentWillUnmount() {
+    console.log('componentWillUnmount on wall')
+  }
 
-    componentDidMount()
-    {
-        this.refreshPosts();
-    }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('shouldComponentUpdate on wall')
+    console.log(nextProps)
+    console.log(nextState)
+    return true
+  }
 
-    refreshPosts(){
-        let username = AuthenticationService.getLoggedInUserName()
-        WallDataService.retrieveAllVisiblePosts(username)
-        .then(response=>this.setState({
-            posts:response.data
-        }))
-          console.log(this.state)
-    }
+  componentDidMount()
+  {
+    this.refreshPosts();
+  }
 
-    editPostButton(postID){
-        console.log('update post' + postID)
-        this.props.history.push(`wall/${postID}`)
-    }
+  refreshPosts(){
+    let username = AuthenticationService.getLoggedInUserName()
+    WallDataService.retrieveAllVisiblePosts(username)
+    .then(response=>this.setState({
+      posts:response.data
+    }))
+    console.log(this.state)
+  }
+
+  editPostButton(postID){
+    console.log('update post' + postID)
+    this.props.history.push(`wall/${postID}`)
+  }
 
 
-    deletePostButton(postID){
-        console.log('delete post' + postID)
-        let username = AuthenticationService.getLoggedInUserName()
-        WallDataService.deletePost(username, postID)
-        .then(response=>{
-            this.refreshPosts()
-        })
-    }
+  deletePostButton(postID){
+    console.log('delete post' + postID)
+    let username = AuthenticationService.getLoggedInUserName()
+    WallDataService.deletePost(username, postID)
+    .then(response=>{
+      this.refreshPosts()
+    })
+  }
 
-    addPostClicked(){
-        console.log("new post button clicked")
-        this.props.history.push(`/wall/-1`)
-    }
+  addPostClicked(){
+    console.log("new post button clicked")
+    this.props.history.push(`/wall/-1`)
+  }
 
-    render(){
-      // let username = AuthenticationService.getLoggedInUserName()
-        const userImage = "http://placekitten.com/300/200"
-        return (
-            
-            
-                   <div>
-                       <div><button className="newPostButton" onClick={this.addPostClicked}> Create New Post :)</button></div>
-                   {
-                       this.state.posts.map((post)=>(
-                        <div  className="postContainer">
-                        <div className="userImage">
-                        <img className ="profilePic" src={userImage} alt="Profile Pic"></img>
+  render(){
+    let username = AuthenticationService.getLoggedInUserName()
+    const userImage = "http://placekitten.com/300/200"
+    return (
 
-                            <div className ="timeStamp">{moment(post.creationTime).format('DD-MM-YYYY HH:MM')}</div>
-                        </div>
-                        {/*<div className="id"> {console.log(post.postID)} </div>*/ }
-                        <div>
-                            <button className ="deleteButton" onClick ={()=>this.deletePostButton(post.postID)}>Delete</button>
-                        </div>
-                        <div>
-                            <button className ="editButton" onClick ={()=>this.editPostButton(post.postID)}>Edit</button>
-                        </div>
-                        <div className="userInput">{post.message}</div>
-                        <hr></hr>
-                       <div>{/*<BottomBar />*/}</div>
-                        
-                        <div>{
-                            (typeof(post.comments)=='object')? 
-                            <div>
-                                {
-                                    post.comments.map((commentBox)=>
-                                    <div>
-                                        <hr></hr>
-                                <div>{/*<CommentBox commentbox={commentBox}/>*/}</div>
-                                    </div>)
-                                }
-                            </div>: null
-                        }</div>
-                    </div>
-                    ))
-                   }
 
-                    </div>
-               
-        );
-     }
+      <div>
+      <div><button className="newPostButton" onClick={this.addPostClicked}> Create New Post :)</button></div>
+      {
+        this.state.posts.map((post)=>(
+          <div  className="postContainer">
+          <div className="userImage">
+          <img className ="profilePic" src={userImage} alt="Profile Pic"></img>
+          <div className="userName">{username}</div>
+
+          </div>
+          {/*<div className="id"> {console.log(post.postID)} </div>*/ }
+
+          <div className="userInput">{post.message}</div>
+          <hr></hr>
+          <div className ="timeStamp">{moment(post.creationTime).format('DD-MM-YYYY HH:MM')}</div>
+          <div className="postSetting">
+          <button className ="editButton" onClick ={()=>this.editPostButton(post.postID)}>Edit</button>
+          <button className ="deleteButton" onClick ={()=>this.deletePostButton(post.postID)}>Delete</button>
+          </div>
+          <hr></hr>
+          <div>{/*<BottomBar />*/}</div>
+
+          <div>{
+            (typeof(post.comments)=='object')?
+            <div>
+            {
+              post.comments.map((commentBox)=>
+              <div>
+              <hr></hr>
+              <div>{/*<CommentBox commentbox={commentBox}/>*/}</div>
+              </div>)
+            }
+            </div>: null
+          }</div>
+          </div>
+        ))
+      }
+
+      </div>
+
+    );
+  }
 
 }
 
